bd233764e9a8d3bf3e141ec8ac289375159a1453
Santosh <santosh.kr.yd.sky@gmail.com>
2026-02-13 13:33:13 +0530

    wellcome

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Custom middleware function that adds req.user property
+const addUserMiddleware = (req, res, next) => {
+  req.user = "Guest";
+  next();
+};
+
+// Route with middleware applied
+app.get('/welcome', addUserMiddleware, (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
 });
